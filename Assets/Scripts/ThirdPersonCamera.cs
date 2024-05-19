f2455b6387d8ccca8b6e9a0091d6760c1f121eeb
umazmot <umazmot@gmail.com>
2024-05-18 21:09:14 -0400

    Added camera zoom in/out

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
@@ -12,7 +13,7 @@ public class ThirdPersonCamera : MonoBehaviour
     // Player object to be followed
     public GameObject target;
     // Camera rotation sensitivity
-    public float sensitivity = 1000f;
+    public float rotSensitivity = 1000f;
     // Camera initial position
     private Vector3 initPos;
     // Horizontal rotation angle
@@ -23,6 +24,10 @@ public class ThirdPersonCamera : MonoBehaviour
     private float yRotMax = 60f;
     // Minimum angle for vertical rotation
     private float yRotMin = -60f;
+    private float distance = 1f;
+    private float maxDistance = 2f;
+    private float minDistance = 0.5f;
+    private float zoomSensitivity = 100f;
 
     void Start()
     {
@@ -31,12 +36,15 @@ public class ThirdPersonCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        xRot += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        yRot -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        distance += Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity * Time.deltaTime;
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+        xRot += Input.GetAxis("Mouse X") * rotSensitivity * Time.deltaTime;
+        yRot -= Input.GetAxis("Mouse Y") * rotSensitivity * Time.deltaTime;
         yRot = Mathf.Clamp(yRot, yRotMin, yRotMax);
 
         Quaternion rot = Quaternion.Euler(yRot, xRot, 0f);
-        Vector3 pos = target.transform.position + rot * initPos;
+        Vector3 pos = target.transform.position + rot * (distance * initPos);
         transform.SetPositionAndRotation(pos, rot);
     }
 }
