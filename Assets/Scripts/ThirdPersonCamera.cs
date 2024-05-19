eb29d2e71912fba0848bdb6da12e126494d2c6ae
umazmot <umazmot@gmail.com>
2024-05-18 21:40:38 -0400

    Refactored

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -12,10 +12,16 @@ public class ThirdPersonCamera : MonoBehaviour
 {
     // Player object to be followed
     public GameObject target;
-    // Camera rotation sensitivity
-    public float rotSensitivity = 1000f;
-    // Camera initial position
+    // Initial position
     private Vector3 initPos;
+    // Zoom scale i.e. distance from the target
+    private float zoomScale = 1f;
+    // Maximum zoom scale
+    private float maxZoomScale = 2f;
+    // Minimum zoom scale
+    private float minZoomScale = 0.5f;
+    // Zoom sensitivity
+    private float zoomSensitivity = 100f;
     // Horizontal rotation angle
     private float xRot = 0f;
     // Vertical rotation angle
@@ -24,10 +30,8 @@ public class ThirdPersonCamera : MonoBehaviour
     private float yRotMax = 60f;
     // Minimum angle for vertical rotation
     private float yRotMin = -60f;
-    private float distance = 1f;
-    private float maxDistance = 2f;
-    private float minDistance = 0.5f;
-    private float zoomSensitivity = 100f;
+    // Rotation sensitivity
+    private float rotSensitivity = 1000f;
 
     void Start()
     {
@@ -36,15 +40,15 @@ public class ThirdPersonCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        distance += Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity * Time.deltaTime;
-        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+        zoomScale += Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity * Time.deltaTime;
+        zoomScale = Mathf.Clamp(zoomScale, minZoomScale, maxZoomScale);
 
         xRot += Input.GetAxis("Mouse X") * rotSensitivity * Time.deltaTime;
         yRot -= Input.GetAxis("Mouse Y") * rotSensitivity * Time.deltaTime;
         yRot = Mathf.Clamp(yRot, yRotMin, yRotMax);
 
         Quaternion rot = Quaternion.Euler(yRot, xRot, 0f);
-        Vector3 pos = target.transform.position + rot * (distance * initPos);
+        Vector3 pos = target.transform.position + rot * (zoomScale * initPos);
         transform.SetPositionAndRotation(pos, rot);
     }
 }
